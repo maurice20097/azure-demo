e6fef1ec39ac0d6c57ba04cf83f730ec5ca0389a
mauricehsiao <maurice20097@gmail.com>
2023-05-14 15:13:22 +0800

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import { baidu } from './utils/system/statistics'
-import 'element-plus/lib/theme-chalk/index.css'
-import 'element-plus/lib/theme-chalk/display.css' // 引入基于断点的隐藏类
+import 'element-plus/dist/index.css'
+// import 'element-plus/dist/display.css' // 引入基于断点的隐藏类
 import 'normalize.css' // css初始化
 import './assets/style/common.scss' // 公共css
 import App from './App.vue'
